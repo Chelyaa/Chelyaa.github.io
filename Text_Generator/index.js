22c044e2502f51616a2ac84c48cf73c4795b3c10
Chelyaa <0altazar0@gmail.com>
2017-03-08 00:56:33 +0200

    Fix info block

--- Text_Generator/index.js
+++ Text_Generator/index.js
@@ -11,6 +11,23 @@ var generateDict = function() {
 	dictionary = createDict(str);
 }
 
+var showInfo = function() {
+	var str = document.getElementById('str').value,
+			info = document.getElementById('info-str');
+	str = str.replace(/\-/g, "");
+	str = str.replace(/\,/g, "");
+	str = str.replace(/\.$/g, " %END%");
+	str = str.replace(/\!$/g, " %END%");
+	str = str.replace(/\?$/g, " %END%");
+	str = str.replace(/\./g, " %END% %START%");
+	str = str.replace(/\!/g, " %END% %START%");
+	str = str.replace(/\?/g, " %END% %START%");
+	str = str.split(" ");
+	str.unshift("%START%");
+
+	info.innerHTML = "Количество слов в тексте: " + str.length;
+}
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
