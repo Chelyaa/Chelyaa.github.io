e46a1b301a4957685a111155a5d1e7bf7b85123b
Chelyaa <0altazar0@gmail.com>
2017-03-13 00:38:06 +0200

    Modifed frontend

--- FaceBot/index.js
+++ FaceBot/index.js
@@ -1,7 +1,19 @@
-$.ajax({
-  type: "POST",
-  url: "face-bot.000webhostapp.com/",
-  data: { name: "John", location: "Boston" }
-}).done(function( msg ) {
-  alert( "Data Saved: " + msg );
-});
+function setimage() {
+    var $input = $("#uploadimage");
+    var fd = new FormData;
+
+    fd.append('userfile', $input.prop('files')[0]);
+
+    $.ajax({
+        url: "http://face-bot.000webhostapp.com/",
+        data: fd,
+        ccrossDomain: true,
+        processData: false,
+        contentType: false,
+        type: 'POST',
+        success: function (data) {
+          console.log(data);
+          $('#main').append('<img src="' + data + '"/>');
+        }
+    });
+}
