0d5a320acd817ebd809b92340a73860fb543ae16
Chelyaa <0altazar0@gmail.com>
2017-03-22 20:32:03 +0200

    Write Arcanoid script file

---
+++ Arcanoid/index.js
@@ -0,0 +1,198 @@
+var startFlag = true;
+var Graphics = PIXI.Graphics;
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+renderer.backgroundColor = 0x487aa0;
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+renderer.render(stage);
+
+var platform = new Graphics(),
+		plWidth = 100,
+		plHeight = 20,
+		plX = window.innerWidth/2-plWidth/2,
+		plY = window.innerHeight-50,
+		platformObj = {
+			centerX: plX+plWidth/2,
+			centerY: plY+plHeight/2,
+			x: plX,
+			y: plY,
+			halfWidth: plWidth/2,
+			halfHeight: plHeight/2,
+			width: plWidth,
+			height: plHeight
+		}
+stage.addChild(platform);
+
+var ball = new Graphics();
+		bRadius = 20,
+		bX = window.innerWidth/2;
+		bY = plY-bRadius-2,
+		bVx = 0;
+		bVy = 0,
+		ballObj = {
+			centerX: bX,
+			centerY: bY,
+			x: bX-bRadius,
+			y: bY-bRadius,
+			halfWidth: bRadius,
+			halfHeight: bRadius,
+			width: bRadius*2,
+			height: bRadius*2
+		}
+stage.addChild(ball);
+
+var blocks = new Graphics(),
+		blStartX = 100,
+		blStartY = 100,
+		blMarginH = 10,
+		blMarginV = 10,
+		blM = 10,
+		blN = 10,
+		blWidth = 20,
+		blHeight = 10,
+		blocksObj = new Array(blN);
+for(var i = 0; i < blN; i++) {
+	blocksObj[i] = new Array(blM);
+	for(var j = 0; j < blM; j++) {
+		blocksObj[i][j] = {
+			centerX: blStartX+(blWidth+blMarginH)*j,
+			centerY: bY,
+			x: bX-bRadius,
+			y: bY-bRadius,
+			halfWidth: bRadius,
+			halfHeight: bRadius,
+			width: bRadius*2,
+			height: bRadius*2
+		}
+	}
+}
+
+function update() {
+	renderBlocks();
+	platform.clear();
+	platform.beginFill(0x66CCFF);
+	platform.drawRect(plX, plY, plWidth, plHeight);
+	platform.endFill();
+
+	if(bX + bVx > window.innerWidth-bRadius || bX + bVx < bRadius) {
+		bVx = -bVx;
+	}
+	if(bY + bVy > window.innerHeight-bRadius || bY + bVy < bRadius) {
+		bVy = -bVy;
+	}
+	bX += bVx;
+	bY += bVy;
+	ball.clear();
+	ball.beginFill(0xd35d5d);
+	ball.drawCircle(0, 0, bRadius);
+	ball.endFill();
+	ball.x = bX;
+	ball.y = bY;
+	ballObj.x = bX-bRadius;
+	ballObj.y = bY-bRadius;
+	ballObj.centerX = bX;
+	ballObj.centerY = bY;
+
+	if (hitTestRectangle(platformObj, ballObj)) {
+	  bVy = -bVy;
+	}
+
+	renderer.render(stage);
+	requestAnimationFrame(update);
+}
+update();
+
+function renderBlocks() {
+
+	for(var i = 0; i < blN; i++) {
+
+	}
+}
+
+document.addEventListener("keypress", function(e) {
+	var code = e.charCode || e.keyCode,
+			step = 20;
+	switch(code) {
+		case 100:
+			if(plX + step < window.innerWidth-plWidth) {
+				plX += step;
+			} else {
+				plX = window.innerWidth-plWidth;
+			}
+			break;
+		case 97:
+			if(plX - step > 0) {
+				plX -= step;
+			} else {
+				plX = 0;
+			}
+			break;
+		case 32:
+			if(startFlag) {
+				bVx = random(2, 4);
+				bVy = -random(2, 4);
+				startFlag = false;
+			}
+			break;
+	}
+	platformObj.x = plX;
+	platformObj.y = plY;
+	platformObj.centerX = plX+plWidth/2;
+	platformObj.centerY = plY+plHeight/2;
+});
+
+function random(min, max) {
+	return Math.floor(Math.random() * (max-min) + min);
+}
+
+function hitTestRectangle(r1, r2) {
+
+  //Define the variables we'll need to calculate
+  var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+  //hit will determine whether there's a collision
+  hit = false;
+
+  //Find the center points of each sprite
+  r1.centerX = r1.x + r1.width / 2;
+  r1.centerY = r1.y + r1.height / 2;
+  r2.centerX = r2.x + r2.width / 2;
+  r2.centerY = r2.y + r2.height / 2;
+
+  //Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+
+  //Calculate the distance vector between the sprites
+  vx = r1.centerX - r2.centerX;
+  vy = r1.centerY - r2.centerY;
+
+  //Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+  //Check for a collision on the x axis
+  if (Math.abs(vx) < combinedHalfWidths) {
+
+    //A collision might be occuring. Check for a collision on the y axis
+    if (Math.abs(vy) < combinedHalfHeights) {
+
+      //There's definitely a collision happening
+      hit = true;
+    } else {
+
+      //There's no collision on the y axis
+      hit = false;
+    }
+  } else {
+
+    //There's no collision on the x axis
+    hit = false;
+  }
+
+  //`hit` will be either `true` or `false`
+  return hit;
+};
